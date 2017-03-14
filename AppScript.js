917b1a45d5bb449d73698a5b8ba50f559ff53fbe
Unnamed <pv.unnamed@gmail.com>
2017-03-15 00:08:55 +0200

    1. Integrating CryptoJs to Google Apps Scripts. 2. Snapshot variant of TripleDES/AES Encoding/Decoding with a static password. 3. Getting hash of password with MD5.

--- AppScript.js
+++ AppScript.js
@@ -1,10 +1,127 @@
 function onOpen() {
-    var sheet = SpreadsheetApp.getActiveSpreadsheet();
-    var subMenus = [{name: "Зашифровать", functionName: "encode"}, {name: "Расшифровать", functionName: "decode"}];
-  
-    sheet.addMenu("SpreadSheetEncoder", subMenus);
+    var ui = SpreadsheetApp.getUi();
+
+    ui.createAddonMenu()
+        .addSubMenu(ui.createMenu('Encode')
+            .addItem('DES', 'desEncoding')
+            .addItem('AES', 'aesEncoding'))
+        .addSeparator()
+        .addSubMenu(ui.createMenu('Decode')
+            .addItem('DES', 'desDecoding')
+            .addItem('AES', 'aesDecoding'))
+        .addToUi();   
   };
- 
+  
+function desEncoding(){
+    var key = getKey();
+  
+    var sheet = SpreadsheetApp.getActiveSpreadsheet();
+    var data = sheet.getDataRange().getValues();
+  
+    var newData = new Array();
+    for(i in data){
+      var row = data[i];
+      for (var j=0; j<row.length; j++){
+        if (row[j].length > 0){
+                  Logger.log(row[j]);
+          Logger.log(j);
+          row[j] = CryptoJsLib.CryptoJS.TripleDES.encrypt(row[j], key).toString();
+        }
+      }
+  
+      newData.push(row);
+    }
+    
+    var range = sheet.getDataRange().getA1Notation();    
+    sheet.getDataRange().clearContent();
+          Logger.log(newData.join());
+    sheet.getRange(range).setValues(newData);
+};
+
+function aesEncoding(){
+    var key = getKey();
+  
+    var sheet = SpreadsheetApp.getActiveSpreadsheet();
+    var data = sheet.getDataRange().getValues();
+  
+    var newData = new Array();
+    for(i in data){
+      var row = data[i];
+      for (var j=0; j<row.length; j++){
+        if (row[j].length > 0){
+                  Logger.log(row[j]);
+          Logger.log(j);
+          row[j] = CryptoJsLib.CryptoJS.AES.encrypt(row[j], key).toString();
+        }
+      }
+  
+      newData.push(row);
+    }
+    
+    var range = sheet.getDataRange().getA1Notation();    
+    sheet.getDataRange().clearContent();
+          Logger.log(newData.join());
+    sheet.getRange(range).setValues(newData);
+};
+
+function desDecoding(){
+    var key = getKey();
+  
+    var sheet = SpreadsheetApp.getActiveSpreadsheet();
+    var data = sheet.getDataRange().getValues();
+  
+    var newData = new Array();
+    for(i in data){
+      var row = data[i];
+      for (var j=0; j<row.length; j++){
+        if (row[j].length > 0){
+          row[j] = CryptoJsLib.CryptoJS.TripleDES.decrypt(row[j], key).toString(CryptoJsLib.CryptoJS.enc.Utf8);
+        }
+      }
+  
+      newData.push(row);
+    }
+    
+    var range = sheet.getDataRange().getA1Notation();    
+    sheet.getDataRange().clearContent();
+          Logger.log(newData.join());
+    sheet.getRange(range).setValues(newData);
+};
+
+function aesDecoding(){
+  var key = getKey();
+  
+    var sheet = SpreadsheetApp.getActiveSpreadsheet();
+    var data = sheet.getDataRange().getValues();
+  
+    var newData = new Array();
+    for(i in data){
+      var row = data[i];
+      for (var j=0; j<row.length; j++){
+        if (row[j].length > 0){
+          row[j] = CryptoJsLib.CryptoJS.AES.decrypt(row[j], key).toString(CryptoJsLib.CryptoJS.enc.Utf8);
+        }
+      }
+  
+      newData.push(row);
+    }
+    
+    var range = sheet.getDataRange().getA1Notation();    
+    sheet.getDataRange().clearContent();
+          Logger.log(newData.join());
+    sheet.getRange(range).setValues(newData);
+   
+};
+
+function getKey(){ 
+  var key = "test Password 1";
+  key = CryptoJsLib.CryptoJS.MD5(key).toString();
+  key +=  key.substring(1, 16);
+  Logger.log(key);
+
+  return key;
+};
+
 function encode(){
     var sheet = SpreadsheetApp.getActiveSpreadsheet();
     var data = sheet.getDataRange().getValues();
